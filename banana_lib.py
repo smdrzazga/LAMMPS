11ae08a19fbe1f44a3a963f3a6790c6f488f3b97
smdrzazga <smdrzazga@gmail.com>
2023-06-08 18:04:13 +0200

    add Screen and Pixel classes

--- banana_lib.py
+++ banana_lib.py
@@ -1,5 +1,8 @@
 from math import sqrt
 import numpy as np
+import functools
+from scipy.sparse.linalg import eigsh
+import scipy.sparse.linalg
 
 def is_molecule_torn(molecule, axis):
     
@@ -95,13 +98,15 @@ def read_boundaries(file):
 
 def read_number_of_atoms(location):
     with open(location, "r") as f:
-
         i = 0
-        for line in f:
-            l = line.strip().split()
+        for lines in f:
             i += 1
+            if i == 4 :
+                return int(lines.strip().split()[0])
+            else:
+                continue
 
-    return int(l[0])
+
 
 
 def find_max(file, coord):
@@ -164,11 +169,20 @@ def scale(target_volume, current_volume, num_walls):
 
 class Atom:
     def __init__(self, id, type, x, y, z):
-        self.id = id
+        self.id = int(id)
         self.type = type
-        self.x = x
-        self.y = y
-        self.z = z
+        self.position = np.array([float(x), float(y), float(z)])
+
+    def __repr__(self) -> str:
+        return f"{self.id} | {self.type} | {self.position}"
+
+    def is_center(self) -> bool:
+        _center_id = 6
+        _atoms_in_molecule = 11
+        if int(self.id) % _atoms_in_molecule == _center_id:
+            return True
+        else:
+            return False
     
     volume = 1.333 * 3.14 * 0.5 * 0.5 * 0.5
 
@@ -194,27 +208,31 @@ class Simulation_box:
         self.volume = self.x * self.y * self.z
 
 class Molecule:
-    def __init__(self, id):
+    def __init__(self, id, num_atoms):
         self.id = id   
         self.comp = []
+        self.atoms = num_atoms
 
-    atoms = 11
+    def center(self):
+        return self.comp[self.atoms//2]
+
+
     def center_of_mass(self):
         if len(self.comp) != self.atoms:
             return "Error: molecule not fully read." 
 
-        x_com = 0
-        y_com = 0
-        z_com = 0
+        com = np.zeros(3)
 
         for i in range(self.atoms):
-            x_com += self.comp[i].x
-            y_com += self.comp[i].y
-            z_com += self.comp[i].z
+            com += self.comp[i].position
             
-        return Vector(x_com/self.atoms, y_com/self.atoms, z_com/self.atoms)
-    
-    
+        return com / self.atoms
+
+
+    def polarization(self):
+        return self.comp[self.atoms//2].position - (self.comp[-1].position - self.comp[0].position) / 2
+
+
     def shift(self, x, y, z):
         m = self.atoms // 2
         mid = Vector(self.comp[m].x, self.comp[m].y, self.comp[m].z )
@@ -258,9 +276,21 @@ class Molecule:
             self.comp[i].y = temp_x * np.sin(phi) + temp_y * np.cos(phi)
             # self.comp[i].z = self.comp[i].z 
 
-    def print_molecule(self):
+
+    def print(self):
         for i in range(Molecule.atoms):
-            print("%.5f, %.5f, %.5f" % (self.comp[i].x, self.comp[i].y, self.comp[i].z))
+            print(f"{self.comp[i].position}")
+        
+
+    def director(self):
+        director = self.comp[-1].position - self.comp[0].position
+        director /= np.linalg.norm(director)
+        
+        return director
+
+
+    def add(self, atom):
+        self.comp.append(atom)
 
 class Rescale:
     def __init__(self, current_packing_fraction, target_packing_fraction):
@@ -271,4 +301,197 @@ class Rescale:
     target_volume = 1
     current_volume = 1
     factor = 1
+
+
+class Line:
+    def __init__(self, id, type, x, y, z):
+        self.id = int(id)
+        self.type = type
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z) 
+    
+    def is_center(self) -> bool:
+        _center_id = 6
+        _atoms_in_molecule = 11
+        if int(self.id) % _atoms_in_molecule == _center_id:
+            return True
+        else:
+            return False
+
+
+class Pixel:
+    def __init__(self):
+        self.components = []
+
+    def assign(self, atom: Atom):
+        self.components.append(atom.position)
+
+    def mean(self) -> float:
+        # mean = functools.reduce(lambda x,y: x+y, self.components) / len(self.components)
+        
+        mean = 0
+        # check for zero-division
+        if len(self.components) > 0:
+            mean = np.mean(self.components, axis=0)
+            mean /= len(self.components)
+
+        return mean
+    
+    def colour(self) -> float:
+        return 1
+
+    def merge_pixels(self, new_pixel) -> None:
+        self.components.extend(new_pixel.components)
+
+
+class CenterPixel(Pixel):
+    def colour(self):
+        return len(self.components)
+
+
+class DirectorPixel(Pixel):
+    def __init__(self):
+        super().__init__()
+
+    def Q(self):
+        Q = np.zeros((3,3))
+        
+        for director in self.components:
+            Q += 0.5 * (3*np.tensordot(director, director, axes=0) - np.identity(3))
+        
+        if len(self.components) > 0:
+            Q /= len(self.components)
+
+        return Q
+
+    def local_director(self):
+        if len(self.components) == 0:
+            return np.zeros(3)
+        
+        eigenvalue, eigenvector = scipy.sparse.linalg.eigsh(self.Q(), k=1, which="LM")
+        eigenvector = np.reshape(eigenvector, -1)
+
+        return eigenvector
+
+
+    def P2Value(self, direc=[1,1,1], trunc=True):
+        direction = np.array(direc) / np.linalg.norm(np.array(direc))
+
+        # if pixel is empty then return any value
+        if len(self.components) == 0:
+            return 0
+        
+        # Legendre 2nd polynomial with respect to dot product (cosine) between average pixel vector and provided direction
+        p2value = 0.5 * (3 * ( np.dot(self.local_director(), direction) )**2 - 1)
+
+        if trunc:
+            # rescale P2Value from -0.5 to 1, to range 0 to 1
+            p2value = (p2value + 0.5) / 1.5 
+
+        return p2value
+    
+    def colour(self):
+        return self.P2Value()
+    
+
+class Screen:
+    def __init__(self, x, y, pixel_class):
+        self.x = x
+        self.y = y
+        self.screen = [[pixel_class() for j in range(x)] for i in range(y)]
+
+    def __repr__(self) -> str:
+        rows = str()
+        for i in range(self.y):
+            for j in range(self.x):
+                cell = f"{len(self.screen[i][j].components)} "
+                rows = rows + cell
+            rows = rows + "\n"
+        return rows
+
+    def determine_pixel(self, atom: Atom, box: Simulation_box, plane='xz') -> tuple[int, int]:
+        match plane:
+            case "xz":
+                box1 = box.x
+                box2 = box.z
+                atom_position1 = atom.position[0]
+                atom_position2 = atom.position[2]
+
+            case "xy":
+                box1 = box.x; box2 = box.y
+                atom_position1 = atom.position[0]
+                atom_position2 = atom.position[1]
+        
+            case "yz":
+                box1 = box.y
+                box2 = box.z
+                atom_position1 = atom.position[1]
+                atom_position2 = atom.position[2]
+    
+        self._bin_size_1 = box1 / self.x
+        self._bin_size_2 = box2 / self.y
+        
+        bin1 = int(atom_position1 // self._bin_size_1)
+        bin2 = int(atom_position2 // self._bin_size_2)
+
+        return bin1, bin2
+
+    def assign(self, atom: Atom, x, y):
+        self.screen[y][x].assign(atom)
+
+    def append_screenshot(self, screenshot: list):
+        for i in range(self.y):
+            for j in range(self.x):
+                self.screen[i][j].merge_pixels(screenshot.screen[i][j])
+    
+    def colour(self):
+        colour = np.zeros((self.x, self.y))
+
+        for i in range(self.y):
+            for j in range(self.x):
+                colour[i, j] = self.screen[i][j].colour()
+
+        return colour
+
+    def avg_director(self):
+        if not isinstance(self.screen[0][0], DirectorPixel):
+            raise Exception("Pixel has to be Director Pixel")
+
+        Q = np.zeros((3,3))
+        for i in range(self.y):
+            for j in range(self.x):
+                Q += self.screen[i][j].Q()
+
+        Q /= (self.x * self.y)**2
+        _, avg_director = scipy.sparse.linalg.eigsh(Q, k=1, which="LA")
+        avg_director = np.reshape(avg_director, -1)
+
+        return avg_director
+
+
+class Screenshot(Screen):
+    def __init__(self, x, y, pixel_class):
+        super().__init__(x, y, pixel_class)
+
+
+    def pixels_to_scroll(self, pixel_num, box: Simulation_box, drift) -> int:
+        # determine numer of pixels that the image should be scrolled in order to get smectic interferences at the same place
+        return int(pixel_num * drift / box.z)
+    
+    
+    def scroll(self, pixels_to_scroll: int) -> None:
+        # scrolling as slicing screenshot into two pieces and changing their order 
+        self.screen = self.screen[pixels_to_scroll:] + self.screen[:pixels_to_scroll]
+
+
+
+def wrap_atom_to_box(atom: Atom, box: Simulation_box) -> Atom:
+    atom.position[0] = atom.position[0] % box.x
+    atom.position[1] = atom.position[1] % box.y
+    atom.position[2] = atom.position[2] % box.z
+
+    return atom
+
+        
 
